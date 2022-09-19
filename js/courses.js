f87a2924cce3720aa803d59e66bba12208c65b1c
sanjid073 <sanjidmahe@gmail.comm>
2022-09-20 02:06:36 +0600

    course page done

---
+++ js/courses.js
@@ -0,0 +1,14 @@
+function viewMore() {
+  document.getElementById("overflow-list").style.display = "block";
+  document.getElementById("view-more-button-1").style.display = "block";
+  document.getElementById("view-more-button").style.display = "none";
+}
+function viewMoreX() {
+  document.getElementById("overflow-list").style.display = "none";
+  document.getElementById("view-more-button-1").style.display = "none";
+  document.getElementById("view-more-button").style.display = "block";
+}
+function readMore() {
+  document.getElementById("banner-text-1").style.display = "block";
+  document.getElementById("read-btn").style.display = "none";
+}
---
+++ test.js
@@ -0,0 +1,11 @@
+
+function viewMore() {
+    document.getElementById("overflow-list").style.display ="block";
+    document.getElementById("view-more-button-1").style.display ="block";
+    document.getElementById("view-more-button").style.display ="none";
+}
+function viewMoreX() {
+    document.getElementById("overflow-list").style.display ="none";
+    document.getElementById("view-more-button-1").style.display ="none";
+    document.getElementById("view-more-button").style.display ="block";
+}
